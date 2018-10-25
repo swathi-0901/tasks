e6474d75182dec33e0965379637a23c9fc1b63b5
swathi-0901 <43814493+swathi-0901@users.noreply.github.com>
2018-10-25 16:18:58 +0530

    mail using python3 check

--- 1stmail.py
+++ 1stmail.py
@@ -4,11 +4,16 @@ server = smtplib.SMTP('smtp.gmail.com',587)
 
 server.starttls()
 
-server.login("swathi.kasikala@gmail.com","swathi123")
+log_id = input(" Enter the username  ")
+login_key = input("Enter the password ")               
 
+server.login(log_id , login_key)
 
-content = "a sample mail sent with my python script"
+sender_id = input("Enter the sender mail ID ")
+receiver_id = input("Enter the receiver mail ID ")
+content = input("Enter the content of your mail  ")
 
-server.sendmail("swathi.kasikala@gmail.com","swathi.kasikala@gmail.com",content)
+server.sendmail(sender_id,receiver_id,content)
+
 print("email is send successfully")
 server.quit()
