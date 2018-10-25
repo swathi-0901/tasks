69e20814eea7138fc0a8bcb4909a623e214e31f7
swathi-0901 <43814493+swathi-0901@users.noreply.github.com>
2018-10-25 15:51:05 +0530

    Update 1sttweet.py

--- 1sttweet.py
+++ 1sttweet.py
@@ -11,5 +11,5 @@ auth = tweepy.OAuthHandler(api_key,apisecret_key)
 auth.set_access_token(access_token,asecret_token)
 auth.secure = True
 api = tweepy.API(auth)
-tweet1 = " hello this is my 1st tweet - just to check with my python script"
+tweet1 = input(" enter the tweet you what to ...")
 api.update_status(status = tweet1)
