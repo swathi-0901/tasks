826e07dec881848cf519b9c5dc2c03a82b201a4f
swathi-0901 <43814493+swathi-0901@users.noreply.github.com>
2018-10-26 19:19:58 +0530

    Update 1sttweet.py

--- 1sttweet.py
+++ 1sttweet.py
@@ -1,11 +1,6 @@
 import tweepy
 
-api_key = "JUuAx42iwjEhHPvBkrSrWFVqO"
-apisecret_key = "TbOouI9UXJGFhgK6BExvS1GbhFqpwv7fUEd6AREm8j05BFGlmj"
-access_token = "1053163840794312704-WaAk6sWEBApTtbyVXfpNRRHhM23amL"
-asecret_token = "x7ae5w3SbNS0lrWIJ3hccjc1TkwjBFN5GYRlVVWijsVo9"
-
-
+from tokens import *
 
 auth = tweepy.OAuthHandler(api_key,apisecret_key)
 auth.set_access_token(access_token,asecret_token)
